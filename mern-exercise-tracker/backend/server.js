e12dbd0d381bd452179061a00f35c0c77e5260ff
Cole McGuire <colejmcguire45@gmail.com>
2022-06-22 11:57:12 -0400

    Establishes MongoDB connection

--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -12,4 +12,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//Establish MongoDB connection
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open', () => console.log('MongoDB database connection established successfully'));
+
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
